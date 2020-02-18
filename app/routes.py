0587367cffe4a018d24a7a20e1297c7bfa239644
yash gaur <yash.gaur@hashedin.com>
2020-02-18 10:47:42 +0530

    added the method to check what all users are present in the database

--- app/routes.py
+++ app/routes.py
@@ -6,28 +6,33 @@ from app.models import User
 
 @app.route('/profile/<username>/<email>', methods=['PUT', 'POST', 'GET', 'DELETE'])
 def http_method(username, email):
-    if request.method == 'PUT':
+    if request.method == 'PUT':                                                  #update operation
         new_email = 'my_new_email@example.com'
         user = User.query.filter_by(username=username, email=email).first()
         user.email = new_email
         db.session.commit()
         return 'updated email id is ' + new_email
 
-    if request.method == 'GET':
+    if request.method == 'GET':                                                   #read operation
         user = User.query.filter_by(username=username, email=email)
         user = user.first()
         return 'email is {}'.format(user.email)
 
-    if request.method == 'DELETE':
+    if request.method == 'DELETE':                                                 #delete operation
         user = User.query.filter_by(username=username, email=email).delete()
         db.session.commit()
         return 'success deletion with username {}'.format(username)
 
-    if request.method == 'POST':
+    if request.method == 'POST':                                                  #create operation
         u = User(username=username, email=email)
         db.session.add(u)
         db.session.commit()
         return 'create new user with email {}'.format(email)
 
 
-
+@app.route('/xyz', methods=['GET'])
+def xyzmethod():                                                                  #to check what all users are added in the database
+    if request.method == 'GET':
+        app.logger.info("xyz is called")
+        u = User.query.all()
+        return 'user list {}'.format(u)
