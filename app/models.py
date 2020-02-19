c518ba63bd83f6802908898eed1d2a0dff5c228a
yash gaur <yash.gaur@hashedin.com>
2020-02-19 21:16:48 +0530

    milestone 4 task 1 completed all the data is imported from csv and imported in database

--- app/models.py
+++ app/models.py
@@ -3,14 +3,14 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True)
-    email = db.Column(db.String(120), unique=True)
+    username = db.Column(db.String(64))
+    email = db.Column(db.String(120))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
 
-class StaticEmailTemplate(db.Model):                                 #email template class
+class StaticEmailTemplate(db.Model):  # email template class
     eid = db.Column(db.Integer, primary_key=True)
     subject = db.Column(db.String())
     header = db.Column(db.String())
@@ -18,3 +18,63 @@ class StaticEmailTemplate(db.Model):                                 #email temp
 
     def __repr__(self):
         return '<Email Template is {}>'.format(self.header + self.body)
+
+
+class Customer(db.Model):
+    cid = db.Column(db.Integer, primary_key=True)
+    c_first_name = db.Column(db.String())
+    c_last_name = db.Column(db.String())
+    c_gender = db.Column(db.String())
+    c_email = db.Column(db.String())
+    c_age = db.Column(db.Integer)
+    c_address = db.Column(db.String())
+    c_state = db.Column(db.String())
+    c_zipcode = db.Column(db.Integer)
+    c_phonenumber = db.Column(db.String())
+    c_registrationdate = db.Column(db.String())
+
+    def __repr__(self):
+        return "Customer details Updated"
+
+
+class Orders(db.Model):
+    oid = db.Column(db.Integer, primary_key=True)
+    purchase_date = db.Column(db.String())
+    total_price = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "order details"
+
+
+class Product(db.Model):
+    pid = db.Column(db.Integer, primary_key=True)
+    product_sku = db.Column(db.String())
+    product_name = db.Column(db.String())
+    product_brand = db.Column(db.String())
+    product_description = db.Column(db.String())
+    product_color = db.Column(db.String())
+    product_unitprice = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "product details"
+
+
+class Reviews(db.Model):
+    rid = db.Column(db.Integer, primary_key=True)
+    user_product_id = db.Column(db.Integer)
+    product_rating = db.Column(db.String())
+    review_title = db.Column(db.String())
+
+    def __repr__(self):
+        return "review details"
+
+
+class UserProductList(db.Model):
+    user_product_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer)
+    product_id = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+    order_id = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "UserProductList updated"
--- app/routes.py
+++ app/routes.py
@@ -1,30 +1,33 @@
+import csv
+import io
+
 from flask import request
 from flask_mail import Message
 
 from app import app, db, mail
-from app.models import User, StaticEmailTemplate
+from app.models import User, StaticEmailTemplate, UserProductList, Customer, Orders, Product, Reviews
 
 
 @app.route('/profile/<username>/<email>', methods=['PUT', 'POST', 'GET', 'DELETE'])
 def http_method(username, email):
-    if request.method == 'PUT':                                                          # update operation
+    if request.method == 'PUT':  # update operation
         new_email = 'my_new_email@example.com'
         user = User.query.filter_by(username=username, email=email).first()
         user.email = new_email
         db.session.commit()
         return 'updated email id is ' + new_email
 
-    if request.method == 'GET':                                                           # read operation
+    if request.method == 'GET':  # read operation
         user = User.query.filter_by(username=username, email=email)
         user = user.first()
         return 'email is {}'.format(user.email)
 
-    if request.method == 'DELETE':                                                         # delete operation
+    if request.method == 'DELETE':  # delete operation
         user = User.query.filter_by(username=username, email=email).delete()
         db.session.commit()
         return 'success deletion with username {}'.format(username)
 
-    if request.method == 'POST':                                                            # create operation
+    if request.method == 'POST':  # create operation
         u = User(username=username, email=email)
         db.session.add(u)
         db.session.commit()
@@ -32,7 +35,7 @@ def http_method(username, email):
 
 
 @app.route('/xyz', methods=['GET'])
-def xyzmethod():                                                                          # to check what all users are added in the database
+def xyzmethod():  # to check what all users are added in the database
     if request.method == 'GET':
         app.logger.info("xyz is called")
         u = User.query.all()
@@ -52,7 +55,7 @@ def email_template():
     return new_email_template.header + new_email_template.body
 
 
-@app.route('/mail/<eid>', methods=['GET'])                                                     # method to create static email template
+@app.route('/mail/<eid>', methods=['GET'])  # method to create static email template
 def index(eid):
     if request.method == 'GET':
         users = User.query.all()
@@ -67,7 +70,7 @@ def index(eid):
 
 
 @app.route('/dynamic-email', methods=['GET'])
-def dynamic_mail():                                                                             #method to create dynamic email template
+def dynamic_mail():  # method to create dynamic email template
     if request.method == 'GET':
         users = User.query.all()
         for rr in users:
@@ -75,6 +78,80 @@ def dynamic_mail():                                                             
             msg.body = "<h1>New Year Sale</h1><p>hi {}</p><p>Wish you a very happy new year. As you have done " \
                        "shopping worth 7000 this year,we have surprise for you.</p><p>Below is a coupon for this new " \
                        "year sale to bring more joy to in the coming year</p>".format(rr.username)
-            msg.html = msg.body
+            msg.html = msg.body  # to display the body of the mail in html format
             mail.send(msg)
         return "Email Sent"
+
+
+@app.route('/customerList', methods=['POST'])
+def updating_customer_list():
+    if request.method == 'POST':
+        csv_file = request.files['filename']
+        csv_file = io.TextIOWrapper(csv_file, encoding="UTF-8")
+        next(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            customerlist = Customer(cid=row[0], c_first_name=row[1], c_last_name=row[2], c_gender=row[3],
+                                    c_email=row[4], c_age=row[5], c_address=row[6], c_state=row[7], c_zipcode=row[8],
+                                    c_phonenumber=row[9], c_registrationdate=row[10])
+            db.session.add(customerlist)
+        db.session.commit()
+        return "customer list updated"
+
+
+@app.route('/orderList', methods=['POST'])
+def updating_order_list():
+    if request.method == 'POST':
+        csv_file = request.files['filename']
+        csv_file = io.TextIOWrapper(csv_file, encoding="UTF-8")
+        next(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            orderlist = Orders(oid=row[0], purchase_date=row[1], total_price=row[2])
+            db.session.add(orderlist)
+        db.session.commit()
+        return "order list updated"
+
+
+@app.route('/productList', methods=['POST'])
+def updating_product_list():
+    if request.method == 'POST':
+        csv_file = request.files['filename']
+        csv_file = io.TextIOWrapper(csv_file, encoding="UTF-8")
+        next(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            productlist = Product(pid=row[0], product_sku=row[1], product_name=row[2], product_brand=row[3],
+                                  product_description=row[4], product_color=row[5], product_unitprice=row[6])
+            db.session.add(productlist)
+        db.session.commit()
+        return "product list updated"
+
+
+@app.route('/reviewList', methods=['POST'])
+def updating_review_list():
+    if request.method == 'POST':
+        csv_file = request.files['filename']
+        csv_file = io.TextIOWrapper(csv_file, encoding="UTF-8")
+        next(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            reviewlist = Reviews(rid=row[0], user_product_id=row[1], product_rating=row[2], review_title=row[3])
+            db.session.add(reviewlist)
+        db.session.commit()
+        return "review list updated"
+
+
+@app.route('/userProductList', methods=['POST'])  # to import the User product list csv file
+def updating_user_product_list():
+    if request.method == 'POST':
+        csv_file = request.files['filename']
+        csv_file = io.TextIOWrapper(csv_file, encoding="UTF-8")
+        next(csv_file)
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            userproductlist = UserProductList(user_product_id=row[0], user_id=row[1], product_id=row[2],
+                                              quantity=row[3], order_id=row[4])
+            db.session.add(userproductlist)
+        db.session.commit()
+        return "user product list updated"
